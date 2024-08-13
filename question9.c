e1a8590c84fff084786cc643f7f7c7fe3b159ee8
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-08-14 00:18:40 +0530

    Add files via upload
    Write a program to calculate area of a rectangle. Input appropriate data from the user.

---
+++ question9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int l;
+    printf("Enter the length: ");
+    scanf("%d",&l);
+
+    int b;
+    printf("Enter the breadth: ");
+    scanf("%d",&b);
+
+    printf("Area of rectangle is %d",l*b);
+
+    return 0;
+}
