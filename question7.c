32fb4f3c23f8efed7be78e438aaaaf27ca5200fa
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-08-14 00:13:47 +0530

    Add files via upload
    Write a program to calculate sum of two integers. Number is entered by the user.

---
+++ question7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter the value of first nuber:");
+    scanf("%d",&a);
+
+    int b;
+    printf("\nEnter the value of second nuber:");
+    scanf("%d",&b);
+
+    int c;
+    c=a+b;
+
+    printf("\nSum is %d",c);
+    
+    return 0;
+}
