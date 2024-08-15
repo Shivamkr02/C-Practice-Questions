458572fe5e57c7a715895f3fb0d7f6fa12ffdedc
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-08-15 18:19:26 +0530

    Add files via upload
    WAP to take date date as an input in below given format and convert the date format and display the results as given below.
    
    User Input date format - "DD/MM/YYYY"    (27/11/2022)
    Output format - "Day-DD, Month - MM, Year -YYYY"  (Day - 27 , Month - 07, Year - 2022)

---
+++ question19.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int dd,mm,yyyy;
+    printf("Enter DD/MM/YYYY");
+    scanf("%d/%d/%d",&dd,&mm,&yyyy);
+
+    printf("Day - %d, Month - %d, Year - %d",dd,mm,yyyy);
+
+
+
+    return 0;
+}
