bd99c30b7f99442aba53dee48c2fd35330d98aea
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-08-15 18:14:20 +0530

    Add files via upload
    Write a program to input three characters from the user and display characters with their corresponding ASCII codes.

---
+++ question18.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    char c1,c2,c3;
+    printf("Enter the character: \n");
+    scanf("%c %c %c",&c1,&c2,&c3);
+
+    printf("%d %d %d",c1,c2,c3);
+
+    return 0;
+}
