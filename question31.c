670a45b763d11a351f09525ac36e288983ce150f
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-09-05 00:11:23 +0530

    Add files via upload
    Write a program to input a three digit number and display the sum of the digits.

---
+++ question31.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+
+    int b;
+    printf("Enter first number: ");
+    scanf("%d",&b);
+
+    int c;
+    printf("Enter third number: ");
+    scanf("%d",&c);
+
+    int sum = a+b+c;
+
+    printf("Sum is %d ",sum);
+
+    return 0; 
+}
