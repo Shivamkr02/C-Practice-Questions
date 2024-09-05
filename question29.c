99ffe2e68e83d470e2dcb8c3c209a4742567c573
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-09-05 23:46:10 +0530

    Add files via upload
    Write a program to swap values of two int variables without using third variable and arithematic operators.

--- question29.c
+++ question29.c
@@ -1,1 +1,22 @@
-// Write  aprogram to swap values of two int variables without using third variables and without using arithematic operator.
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+
+    int b;
+    printf("Enter second number: ");
+    scanf("%d",&b);
+
+
+    a = a^b;
+    b = a^b;
+    a= a^b;
+    a = a^b^b;
+    b = a^b^a;
+
+    printf("First number: %d \nSecond number: %d",a,b);
+
+    return 0;
+}
