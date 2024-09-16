20901be59a6fbd3f943f2ec183e1450487dd1ac7
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-09-16 06:43:50 -0700

    Add files via upload
    Write a program which asks user to enter an even number, computer will give user at most 3 chances, if user failed to answer in any of the chances, "Game Over" message should be displayed on the screen, otherwise "You Win" message should be displayed and game stops immediatly.
    
                   WIN  -> Even number in any chance
                   Game Over -> Wrong / Odd input in all the three chances

---
+++ 12.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+   int x,i;
+
+   for(i=1;i<=3;i++){
+    printf("\nEnter even number: ");
+    scanf("%d",&x);
+
+        if(x%2 == 0)
+        break;
+   }
+   if(i == 4)
+    printf("Game Over");
+
+    else
+        printf("You Win");
+
+    return 0;
+}
