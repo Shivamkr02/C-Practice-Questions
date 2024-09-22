593f4ee7982676651cb5fcc90fcd44edd200de71
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-09-22 23:23:42 +0530

    Update question96.c

--- question96.c
+++ question96.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 
 int main(){
-    int n;
-    printf("Enter number: ");
-    scanf("%d",&n);
 
-    int i;
-    for(i=2;i<=n-1;i++){
-        if(n%i == 0){
-        printf("Given number is Not-Prime");
-        break;
-        }
-    }
-    if(i == n){
-        printf("Given number is prime");
+
+    int l,u,x,i;
+    l=2;
+    u=100;
+
+    for(x=l;x<=u;x++){
+
+        for(i=2;i<x;i++)
+            if(x%i==0)
+                break;
+
+        if(i==x)
+            printf("%d\n",x);
+
     }
     return 0;
 }
