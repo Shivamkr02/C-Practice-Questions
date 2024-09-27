4016b9c117e1296baba4241539e4d40530675023
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-09-27 10:59:56 -0700

    Add files via upload
    Write a program to print the following pattern
    
    
    A  B  C  D  E
        B  C  D  E
            C  D  E
                D  E
                    E

---
+++ question109.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int i,j,k;
+
+    for(i='A';i<='E';i++){
+        k=i;
+        for(j='A';j<='E';j++){
+            if(i<=j){
+                printf("%c ",k);
+                k++;
+            }
+            else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
