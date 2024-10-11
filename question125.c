383664547e3fb2db2eb09a79c0af5c2ad3b66861
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-10-12 00:04:27 +0530

    Add files via upload
    Convert the following if-else-if construct into switch case:
    
    
    if(var == 1)
            printf("good");
    else if(var == 2)
            printf("better");
    else if(var == 3)
            printf("best");
    else
           printf("invalid");

---
+++ question125.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int var;
+    printf("Enter the value of var: ");
+    scanf("%d",&var);
+
+    switch(var){
+        case 1:
+            printf("Good");
+            break;
+
+        case 2:
+            printf("better");
+            break;
+
+        case 3:
+            printf("best");
+            break;
+
+        default:
+            printf("invalid");
+            break;
+    }
+}
